5ce75f3ff0a2c49942ac7656dc86ff260b564925
ChristoforosPsychas <37118969+ChristoforosPsychas@users.noreply.github.com>
2018-07-29 19:15:15 +0300

    commit 4

--- src/ListView.js
+++ src/ListView.js
@@ -5,16 +5,39 @@ import escapeRegExp from 'escape-string-regexp'
 class ListView extends Component {
 
   state = {
-    query: ''
+    query: '',
+    searchedLocations: this.props.locations,
+    hideRestMarkers: this.props.markers
   }
 
   updateQuery = (query) => {
 
     this.setState({ query })
 
+    this.updateLocations(query)
+
   }
 
+  updateLocations = (query) => {
+    let searchedLocations
+    let hideRestMarkers
 
+    if (query) {
+      const match =  new RegExp(escapeRegExp(this.state.query), 'i')
+
+      searchedLocations = this.props.locations.filter(location =>
+                match.test(location.title))
+      hideRestMarkers = this.props.markers.filter(marker =>
+                searchedLocations.every(location => location.title !== marker.title))
+      hideRestMarkers.forEach(marker => marker.setVisible(false))
+      this.setState({searchedLocations, hideRestMarkers})
+    } else {
+      this.props.markers.forEach(marker => marker.setVisible(true))
+      this.setState({searchedLocations: this.props.locations, hideRestMarkers: this.props.markers})
+
+    }
+
+  }
 
   openInfo = (e) => {
 
@@ -23,7 +46,8 @@ class ListView extends Component {
  }
 
   render() {
-    const match =  new RegExp(escapeRegExp(this.state.query), 'i')
+
+
 
     /****************************************************************************************************************************
     *There is an attribute that you can add on all DOM element, it's the `data-*`                                               *
@@ -43,25 +67,44 @@ class ListView extends Component {
       />
       <ul>
       {
-        this.props.locations.filter((location, index) => {
-          this.props.markers.filter(marker => {
-           match.test(location.title).filter(marker => {
-             //https://stackoverflow.com/questions/5030127/marker-visibility-in-google-maps
-             marker.title !== location.title
-             marker.setVisible(false)
-           }
-
-         )})
-
-       return(
-            <li
+        // this.props.locations.map((location, index) => {
+        //   this.props.markers.filter(marker => {
+        //    match.test(location.title).filter(marker => {
+        //      //https://stackoverflow.com/questions/5030127/marker-visibility-in-google-maps
+        //      marker.title !== location.title
+        //      marker.setVisible(false)
+        //    }
+        //
+        //  )})
+        this.state.searchedLocations.map((location, index) => (
+          <li
+              key={location.title}
               data-index={index}
               onClick={this.openInfo}
-            >
+          >
               {location.title}
-            </li>
-          )
-      })
+          </li>
+        ))
+
+
+
+
+
+        // this.props.markers.filter(marker => this.props.locations.map((location, index) =>
+        //  match.test(location.title)
+        //
+        //  .filter(location =>
+        // location.title !== marker.title && (
+        //     <li
+        //       data-index={index}
+        //       onClick={this.openInfo}
+        //     >
+        //       {location.title}
+        //     </li>)
+        //   ))
+        // )
+
+      //})
     }
       </ul>
       </div>
